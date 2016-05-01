c156883d3e74a5ddc502d1569a237180bd5c9b4d
mhaseebmlk <17100254@lums.edu.pk>
2016-05-01 07:13:41 +0000

    with html

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401114100) do
+ActiveRecord::Schema.define(version: 20160402050715) do
 
   create_table "customers", force: :cascade do |t|
     t.string  "name"
@@ -40,11 +40,12 @@ ActiveRecord::Schema.define(version: 20160401114100) do
     t.integer  "customer_id"
     t.integer  "item_id"
     t.datetime "placed_at"
-    t.string   "status"
+    t.boolean  "delievered"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "order_id"
     t.integer  "quantity"
+    t.string   "status"
   end
 
   create_table "users", force: :cascade do |t|
